c16ff4c7eaa5ffb2785278864d536f0cc0088e4a
ChrisShep98 <shepardchristopher1@gmail.com>
2024-04-02 20:55:19 -0400

    removed comment

--- src/main/java/CliffZR/climbing/constant/UserListener.java
+++ src/main/java/CliffZR/climbing/constant/UserListener.java
@@ -28,31 +28,3 @@ public class UserListener {
 
     }
 }
-
-
-//@PrePersist
-//public void prePersist(User user) {
-//    String[] gradeTitles = {"v0", "v1", "v2", "v3"};
-//    List<BoulderingGrade> boulderingGrades = new ArrayList<>();
-//    for (String gradeTitle : gradeTitles) {
-//        BoulderingGrade boulderingGrade = new BoulderingGrade();
-//        boulderingGrade.setStatus(false);
-//        boulderingGrade.setImage("imageurlhere");
-//        boulderingGrade.setTitle(gradeTitle);
-//        boulderingGrade.setUser(user);
-//        boulderingGrades.add(boulderingGrade); // Add the bouldering grade to the list
-//    }
-//    user.setBoulderingGrade(boulderingGrades); // Set the list of bouldering grades to the user
-//}
-
-
-//String[] gradeTitles = {"v0", "v1", "v2", "v3"};
-//        for (String gradeTitle : gradeTitles) {
-//BoulderingGrade boulderingGrade = new BoulderingGrade();
-//            boulderingGrade.setStatus(false);
-//            boulderingGrade.setImage("imageurlhere");
-//            boulderingGrade.setTitle(gradeTitle);
-//            boulderingGrade.setUser(user);
-//            user.getBoulderingGrades().add(boulderingGrade); // Add bouldering grade to the list
-//            entityManager.persist(boulderingGrade); // Persist bouldering grade
-//        }
